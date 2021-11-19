cd118be134ec4246c32c137f5a814c780ae347b6
chaudharyakash45 <88129128+chaudharyakash45@users.noreply.github.com>
2021-11-19 15:55:46 +0530

    Add files via upload

---
+++ threadsafe.java
@@ -0,0 +1,23 @@
+
+package com.crunchify;
+ 
+public class CrunchifySingleton {
+ 
+	private static CrunchifySingleton instance = null;
+ 
+	protected CrunchifySingleton() {
+	}
+ 
+	
+	public static CrunchifySingleton getInstance() {
+		if (instance == null) {
+			
+			synchronized (CrunchifySingleton.class) {
+				if (instance == null) {
+					instance = new CrunchifySingleton();
+				}
+			}
+		}
+		return instance;
+	}
+}
